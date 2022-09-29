25e04ece89983db08d908d76e407835fe9edff86
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-09-29 18:07:28 -0400

    Initial Commit

---
+++ BetterRest-HWS-3/BetterRest_HWS_3App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_HWS_3App.swift
+//  BetterRest-HWS-3
+//
+//  Created by Kevin Mattocks on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_HWS_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
