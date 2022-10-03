60620a28e7f2fdff414f5565330ac16e875d7d45
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-10-02 20:24:50 -0400

    Add logic to calculateBedTime method Add properties for alert Add alert to the calculate button in navigation bar Navigation title and calculate button is not displaying on iPhone 14 for some reason

--- BetterRest-HWS-3/ContentView.swift
+++ BetterRest-HWS-3/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Kevin Mattocks on 9/29/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -13,6 +14,10 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     
     var body: some View {
@@ -43,13 +48,40 @@ struct ContentView: View {
                 calculateBedTime()
             }
         }
+        .alert(alertTitle, isPresented: $showingAlert) {
+            Button("OK") { }
+        } message: {
+            Text(alertMessage)
+        }
     }
     
     
     //MARK: - Methods
     
     func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is.."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            
+        } catch {
+            //Something went wrong
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+            
+        }
         
+        showingAlert = true
     }
 }
 
